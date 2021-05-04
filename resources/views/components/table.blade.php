dc38821808d70c6d8681b0fb92351d636e090a4e
Rasmyta <rasmabu@gmail.com>
2021-05-04 22:58:03 +0200

    Update tables style

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,4 +1,5 @@
-<div class="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg">
+<div style="height: 22rem;"
+    class="align-middle min-w-full overflow-y-auto overflow-x-auto shadow overflow-hidden sm:rounded-lg">
     <table class="min-w-full divide-y divide-cool-gray-200">
         <thead>
             <tr class="dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800">
