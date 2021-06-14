01c0da2297c0ed54fd074d9a461f7887cde11bd3
Rasmyta <rasmabu@gmail.com>
2021-06-14 13:46:50 +0200

    Fix welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,13 @@
 <x-app-layout>
-    <div id="welcome-container" style="background-image: url('../images/bg1.jpg');
-            background-size: contain;
-            background-repeat: no-repeat;
-            opacity: 0.5;
-            " class="relative flex items-top justify-end min-h-screen sm:items-center py-4 sm:pt-0">
+    <div class="flex flex-nowrap items-top justify-end min-h-screen sm:items-center sm:pt-0">
 
-        <div class="row1 d-flex justify-end justify-items-end items-center">
+        <div id="welcome-image" class="w-full" style="min-height: 44rem;"></div>
+
+        <div class="d-flex justify-end justify-items-end items-center">
             <div class="w-2/3">
-                <h1 class="text-white text-4xl font-bold text-right">Descubre salones y
+                <h1 class="text-white text-5xl font-bold text-right">Descubre salones y
                     reserva tus citas de belleza</h1>
             </div>
         </div>
-
     </div>
 </x-app-layout>
