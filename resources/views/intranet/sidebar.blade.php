6e253b3ad7efafc921871f45c07d20ac06199453
Rasmyta <rasmabu@gmail.com>
2021-06-17 09:41:03 +0200

    Added welcome route in the intranet sidebar

--- resources/views/intranet/sidebar.blade.php
+++ resources/views/intranet/sidebar.blade.php
@@ -2,7 +2,7 @@
     <!-- Desktop sidebar -->
     <aside class="z-20 hidden h-full overflow-y-auto bg-white dark:bg-gray-800 md:block flex-shrink-0" style="width: 14rem;">
         <div class="py-4 text-gray-500 dark:text-gray-400">
-            <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#">
+            <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="{{ route('welcome') }}">
                 MyComfort
             </a>
             <ul class="mt-6">
